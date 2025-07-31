7d3c3ff866e63a68d36347b247d0e9732e419286
Marco13579751 <106212587+Marco13579751@users.noreply.github.com>
2025-07-31 13:31:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -201,13 +201,13 @@ criticity['VeryHigh'] = fuzz.gaussmf(criticity.universe, 9, 0.7)
 
 
 rules = [
-    ctrl.Rule(normalized_age['New'], criticity['VeryLow']),
-    ctrl.Rule(normalized_age['Middle'], criticity['Medium']),
-    ctrl.Rule(normalized_age['Old'], criticity['VeryHigh']),
+    #ctrl.Rule(normalized_age['New'], criticity['VeryLow']),
+    #ctrl.Rule(normalized_age['Middle'], criticity['Medium']),
+    #ctrl.Rule(normalized_age['Old'], criticity['VeryHigh']),
 
-    ctrl.Rule(normalized_fault_rate_levels['Under trh'], criticity['VeryLow']),
-    ctrl.Rule(normalized_fault_rate_levels['Around trh'], criticity['Medium']),
-    ctrl.Rule(normalized_fault_rate_levels['Above trh'], criticity['VeryHigh']),
+    #ctrl.Rule(normalized_fault_rate_levels['Under trh'], criticity['VeryLow']),
+    #ctrl.Rule(normalized_fault_rate_levels['Around trh'], criticity['Medium']),
+    #ctrl.Rule(normalized_fault_rate_levels['Above trh'], criticity['VeryHigh']),
 
 
     # --- Age: NEW ---
