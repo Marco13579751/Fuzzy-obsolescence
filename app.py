0a13ae203d70880a188cb0352affccc956e4551f
Marco13579751 <106212587+Marco13579751@users.noreply.github.com>
2025-07-31 13:07:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -198,13 +198,13 @@ criticity['VeryHigh'] = fuzz.gaussmf(criticity.universe, 9, 0.7)
 
 # Define fuzzy rules
 rules = [
-    ctrl.Rule(normalized_age['New'], criticity['VeryLow']),
-    ctrl.Rule(normalized_age['Middle'], criticity['Medium']),
-    ctrl.Rule(normalized_age['Old'], criticity['VeryHigh']),
+    ctrl.Rule(normalized_age['New'], criticity['VeryLow'],weigth=0.5),
+    ctrl.Rule(normalized_age['Middle'], criticity['Medium'],weigth=0.5),
+    ctrl.Rule(normalized_age['Old'], criticity['VeryHigh'],weigth=0.5),
     
-    ctrl.Rule(normalized_fault_rate_levels['Under trh'], criticity['VeryLow']),
-    ctrl.Rule(normalized_fault_rate_levels['Around trh'], criticity['Medium']),
-    ctrl.Rule(normalized_fault_rate_levels['Above trh'], criticity['VeryHigh']),
+    ctrl.Rule(normalized_fault_rate_levels['Under trh'], criticity['VeryLow'],weigth=0.5),
+    ctrl.Rule(normalized_fault_rate_levels['Around trh'], criticity['Medium'],weigth=0.5),
+    ctrl.Rule(normalized_fault_rate_levels['Above trh'], criticity['VeryHigh'],weigth=0.5),
 
 
    
