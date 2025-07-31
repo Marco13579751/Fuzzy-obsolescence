0ff734cce694ff9555a26cd063523aedfe7cceba
Marco13579751 <106212587+Marco13579751@users.noreply.github.com>
2025-07-31 09:39:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -167,8 +167,8 @@ criticity = ctrl.Consequent(np.arange(0, 10.1, 0.1), 'Criticity')
 
 # Define membership functions for normalizedAge
 
-normalized_age['New'] = fuzz.trapmf(normalized_age.universe, [0, 0, 0.3, 0.4])
-normalized_age['Middle'] = fuzz.trimf(normalized_age.universe, [0.3, 0.5, 0.7])
+normalized_age['New'] = fuzz.trapmf(normalized_age.universe, [0, 0, 0.4, 0.6])
+normalized_age['Middle'] = fuzz.trimf(normalized_age.universe, [0.5, 0.6, 0.7])
 normalized_age['Old'] = fuzz.trapmf(normalized_age.universe, [0.6, 0.7, 1, 1])
 
 # Define membership functions for normalizedfaultRateLevels
