6dfbaaf0d15949bc6630dccd48994d9598612d57
Marco13579751 <106212587+Marco13579751@users.noreply.github.com>
2025-08-27 18:49:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -519,7 +519,7 @@ criticity_simulation.input['reliability_result'] = reliability_score
 
 #criticity_simulation.compute()
 
-print(criticity_simulation.output['criticity'])
+#print(criticity_simulation.output['criticity'])
 criticity_score=show_fuzzy_output(criticity, criticity_simulation)
 
 
